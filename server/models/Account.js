8bf5750595bca8d0e1440f57784a77eef2ef991b
Jamal Woods <JamalCWoods@Gmail.com>
2021-03-25 20:32:29 -0400

    fixed error for circleCI

--- server/models/Account.js
+++ server/models/Account.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const { resolveSoa } = require('dns');
+//const { resolveSoa } = require('dns');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
